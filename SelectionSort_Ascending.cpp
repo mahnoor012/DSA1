cbddf8b13cfbcbc5034273e9b75559337f351528
MahnoorEsha <142165365+mahnoor012@users.noreply.github.com>
2024-11-05 05:06:32 +0600

    Add files via upload

---
+++ SelectionSort_Ascending.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,m_index;
+    cout<<"Enter array size ";
+    cin>>n;
+    int arr[n];
+    cout<<"Unsorted Array : ";
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+
+    for(i=0; i<=n; i++)
+    {
+        m_index=i;
+        for(j=i+1; j<n; j++)
+        {
+            if(arr[j]<arr[m_index])
+            {
+                m_index=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[m_index];
+        arr[m_index]=temp;
+    }
+
+    cout<<"Sorted array ";//ascending order
+    for(i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
